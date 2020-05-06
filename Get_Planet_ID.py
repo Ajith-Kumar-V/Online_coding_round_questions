e4dd0ac6eea737d70c4f0df8a398a3e56054bd4f
Ajith Kumar V <54568246+Ajith-Kumar-V@users.noreply.github.com>
2020-05-06 09:52:58 +0530

    Create Get_Planet_ID.py

---
+++ Get_Planet_ID.py
@@ -0,0 +1,5 @@
+def get_planet_name(id):
+    switch={1:"Mercury",2:"Venus",3:"Earth",4:"Mars",5:"Jupiter",6:"Saturn",7:"Uranus",8:"Neptune",}
+    print(switch[id])
+val=int(input())
+get_planet_name(val)
